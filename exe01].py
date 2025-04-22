c456ebc9a666b40f31d2c855e43e033b801a7ac4
vinicius abraao <viniciuszeusdd@gmail.com>
2025-04-22 09:07:56 -0300

    exe 03 revisao

---
+++ exe01].py
@@ -0,0 +1,9 @@
+a= int((input("diga o valor A: ")))
+b= int((input("diga o valor B: ")))
+c= int((input("diga o valor C: ")))
+
+soma= a+b
+print(soma)
+
+if soma <c:
+    print(f"o valor da soma {soma} e menor que o valor C")
