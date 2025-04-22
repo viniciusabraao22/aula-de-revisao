3abf182daf5e4a1ea922faec346b0c05cbb7435f
vinicius abraao <viniciuszeusdd@gmail.com>
2025-04-22 10:36:09 -0300

    exe 05 revisaoo

---
+++ exe03.py
@@ -0,0 +1,6 @@
+num=int(input("diga um numero"))
+
+ant= num -1
+suces= num +1
+
+print(f" ant {ant} o suces e {suces}")
---
+++ exe04.py
@@ -0,0 +1,8 @@
+sala=float(input("diga seu salario: "))
+salmin=float(input("diga quanto estar o salario minimo: "))
+devisao= sala/salmin
+
+print(F" voce recebe {devisao} tantos salarios")
+
+
+
